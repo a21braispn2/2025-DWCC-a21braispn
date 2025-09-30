ca3439a101bbe512d82b2091843c0070503a9f03
a21braispn2 <a21braispn@iessanclemente.net>
2025-09-30 11:40:05 +0200

    Exercicios objetos js

--- Prueba/script.js
+++ Prueba/script.js
@@ -1,1 +1,41 @@
-console.log('Ola mundo');
+// 'use strict';
+// // 1. Crea unha variable que almacene un día da semana de luns a domingo. En función
+// // do valor da variable mostra unha mensaxe indicando se o día é laborable ou non.
+
+// let dia = 'Lunes';
+
+// let laborables = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes'];
+// let nonLaborables = ['Sabado', 'Domingo'];
+
+// if (dia in laborables) {
+//   console.log('Laboral');
+// } else if (dia in nonLaborables) {
+//   console.log('Non laboral');
+// } else {
+//   console.log('Escribe un dia da semana');
+// }
+
+// 2. Crea 3 variables e inicialízaas con 3 números diferentes. Mostra por pantalla o maior
+// dos 3 números.
+
+let a = 3;
+let b = 5;
+let c = 2;
+
+if (a > b && a > c) {
+  console.log('a');
+} else if (b > c) {
+  console.log('b');
+} else {
+  console.log('c');
+}
+// 3. Escribe os números pares do 0 ao 30, incluídos.
+// 4. Escribe as potencias de 2, dende 2⁰ ata 220. Para cada potencia debe saír un texto
+// similar a “2 elevado a 0 = 1”.
+// 5. Inicializa unha variable a un número, calcula o seu factorial e mostra a resultado por
+// consola. (5! = 5*4*3*2*1).
+// 6. Cálculo do IMC (índice de masa corporal). IMC = peso (kg) / [estatura (m)]2
+// a. Almacena en variables o peso e altura de dúas persoas.
+// b. Calcula o IMC das dúas persoas.
+// c. Indica que persoa ten o maior IMC cunha cadea similar a: 'O IMC (25.3) da
+// primeira persoa é maior que o da segunda persoa (22.5)!
